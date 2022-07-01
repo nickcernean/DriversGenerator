a3b05ab4e7a9bda4543695dfbe18a78684985bec
nick <nicolaecer@gmail.com>
2022-07-01 09:50:23 +0200

    started the checksum implementation TO DO: add the checksum

---
+++ src/tools/ChecksumCalculator.java
@@ -0,0 +1,37 @@
+package tools;
+
+import jdk.jshell.spi.ExecutionControl;
+
+public class ChecksumCalculator {
+
+
+    public static String Add(String argumentToAdd) {
+
+        return "";
+    }
+
+    public static String BitwiseAND(String argumentToAdd) {
+        return "";
+    }
+
+    public static String CRC_8(String argumentToAdd) {
+        return "";
+    }
+
+    public static String OnesCompliment(String argumentToAdd) {
+        return "";
+    }
+
+    public static String BitwiseOR(String argumentToAdd) {
+        return "";
+    }
+
+    public static String Subtract(String argumentToAdd) {
+        return "";
+    }
+
+    public static String TwosCompliment(String argumentToAdd) {
+        return "";
+    }
+
+}
