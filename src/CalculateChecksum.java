4be7ae0582a75a6d539df09d2cc759a98a4ab988
gabriela.dicusar <gabrieladicusar@gmail.com>
2022-07-15 11:47:16 +0200

    local commit TO DO: fix 0level command error

--- src/CalculateChecksum.java
+++ src/CalculateChecksum.java
@@ -4,8 +4,9 @@ public class CalculateChecksum {
 
 
     public static void main(String[] args) {
+
         //Adding calculation
-        System.out.println("Checksum Add result: " + ChecksumCalculator.Add("14FE0131", 0, 3));
+        System.out.println("Checksum Add result: " + ChecksumCalculator.Add("AA14FE0131", 1, 4));
         System.out.println("Checksum Subtract result: " + ChecksumCalculator.Subtract("14FE0131", 0, 3));
         System.out.println("Checksum BitwiseAND result: " + ChecksumCalculator.BitwiseAND("14FE0131", 0, 3));
         System.out.println("Checksum BitwiseOr result: " + ChecksumCalculator.BitwiseOR("14FE0131", 0, 3));
